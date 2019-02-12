309919415ceccdd755795810615349b73301be43
nikwo <36183206+CCodingNovice@users.noreply.github.com>
2019-02-12 10:47:25 +0300

    gui recap v2

--- gui.py
+++ gui.py
@@ -1,8 +1,10 @@
 from tkinter import *
 from shop_database import create_database
 
+
 def add():
     pass
+
 
 main_window = Tk()
 MENU_LIST = ["Жидкость", "menu 2", "menu 3", "menu 4", "menu 5", "menu 6", "menu 7", "menu 8", "menu 9", "menu 10"]
@@ -10,6 +12,7 @@ MENU_LIST = ["Жидкость", "menu 2", "menu 3", "menu 4", "menu 5", "menu 6
 # CONFIGURE MAIN WINDOW
 main_window.title(u"Vape Shop app")
 main_window.configure(background="#49464c")
+main_window.geometry("800x600")
 
 HELLO_USER = Label(main_window,
                    background="#1c0f21",
@@ -33,7 +36,7 @@ RIGHT_MENU = Listbox(main_window,
 for i in MENU_LIST:
     RIGHT_MENU.insert(END, i)
 
-RIGHT_MENU.grid(row=1, column=5, sticky="ns", rowspan=3)
+RIGHT_MENU.grid(row=1, column=5, sticky="ns", rowspan=5)
 
 Grid.rowconfigure(main_window, 1, weight=1)
 
@@ -65,55 +68,25 @@ AMOUNT_LABEL = Label(main_window, text="amount",
                      ).grid(row=1, column=3, sticky="nwe", columnspan=2)
 Grid.columnconfigure(main_window, 3, weight=1)
 
-id_entry = Entry(main_window, text="id", width=6, background="gray").grid(row=3, column=0, sticky="swe")
-name_entry = Entry(main_window, text="name", width=10, background="gray").grid(row=3, column=1, sticky="swe")
-price_entry = Entry(main_window, text="price", width=6, background="gray").grid(row=3, column=2, sticky="swe")
-amount_entry = Entry(main_window, text="amount", width=10, background="gray").grid(row=3, column=3, sticky="swe")
-ADD_BTN = Button(main_window, text="add",
-                 background="gray",
-                 foreground="orange"
-                 ).grid(row=3, column=4, sticky="swe")
+INFO_LABEL = Label(main_window, text="Insertion into database",
+                   background="#1c0f21",
+                   foreground="orange"
+                   ).grid(row=3, column=0, columnspan=5, sticky="swe")
 Grid.rowconfigure(main_window, 3, weight=1)
 
 
-"""
-
-LEFT_FRAME = Frame(main_window, background="#49464c")
-LEFT_FRAME.grid(row=0, column=0, sticky="nwse")
-
-
-RIGHT_MENU_FRAME = Frame(main_window, bg="#312b38")
-RIGHT_MENU_FRAME.grid(row=0, column=1, sticky="nes")
-
-# CONFIGURE GREETING LABEL
-HELLO_USER = Label(LEFT_FRAME, text="Vape shop app\n"
-                                     "Powered by biv184", font="Arial 14")
-HELLO_USER.configure(background="#1c0f21", fg="orange", height=4)
-HELLO_USER.pack(side="top")
+name_entry = Entry(main_window, text="name", width=10, background="gray", justify=CENTER
+                   ).grid(row=4, column=1, sticky="swe", padx=5)
+price_entry = Entry(main_window, text="price", width=6, background="gray", justify=CENTER
+                    ).grid(row=4, column=2, sticky="swe", padx=5)
+amount_entry = Entry(main_window, text="amount", width=10, background="gray", justify=CENTER
+                     ).grid(row=4, column=3, sticky="swe", padx=5)
+ADD_BTN = Button(main_window, text="add",
+                 background="gray",
+                 foreground="black"
+                 ).grid(row=4, column=4, sticky="swe")
+BOTTOM_EMPTY = Label(main_window, background="#49464c", height=1
+                     ).grid(row=5, column=0, columnspan=5, sticky="nwe")
 
 
-#DATA_LISTBOX = Listbox(LEFT_FRAME, selectmode=SINGLE, fg="orange", side="top")
-
-
-id_entry = Entry(LEFT_FRAME, text="id", width=6).pack(side="left")
-name_entry = Entry(LEFT_FRAME, text="name", width=10).pack(side="left")
-price_entry = Entry(LEFT_FRAME, text="price", width=6).pack(side="left")
-amount_entry = Entry(LEFT_FRAME, text="amount", width=10).pack(side="left")
-
-
-
-
-RIGHT_LABEL = Label(RIGHT_MENU_FRAME, text="\n", bg="#1c0f21", height=4, font="Arial 14")
-
-RIGHT_LISTBOX = Listbox(RIGHT_MENU_FRAME, selectmode=BROWSE, bg="#312b38", fg="orange", relief=FLAT, width=20)
-RIGHT_LISTBOX.config(highlightthickness=0)
-
-for i in MENU_LIST:
-    RIGHT_LISTBOX.insert(END, i)
-
-RIGHT_LABEL.pack(side="top", fill=X)
-HELLO_USER.pack(fill=X)
-RIGHT_LISTBOX.pack(side="top", expand=FALSE)
-RIGHT_LISTBOX.config(font="Arial 12")
-"""
 main_window.mainloop()
