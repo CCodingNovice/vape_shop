04b17e1511b34f3d1ff92903346633ae51690e9d
siraj <efendiev.sirajudin@yandex.ru>
2019-04-21 15:11:38 +0300

    допилил пайчарТы, добавил optionmenu

--- chart.py
+++ chart.py
@@ -14,7 +14,7 @@ print(DF)
 
 
 
-def PriceName():																			# Цена - Имя
+def PriceName():
 	plt.pie(DF['price'], labels= DF['price'], shadow=5, autopct='%1.1f%%')
 	plt.title("Цена - Имя")
 	plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), shadow=True, ncol=2, labels= DF['name'], )
@@ -27,28 +27,84 @@ def AmountName():
 	plt.show()
 
 
-#def AmountType():
-#	plt.pie(DF['amount'], labels=DF['amount'], shadow=5, autopct='%1.1f%%')
-#	plt.title("Количество - Тип")
-#	plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), shadow=True, ncol=2, labels=DF['type'], )
-#	plt.show()
+def AmountType():
+
+	icol=0
+
+	typecol1 = [] # список содержащий элементы type без повторений
+	amountcol = [] # список содержащий количество вейпов всех типов без повторений
+
+	for i in DF['type']:
+		j = 0
+		flag = False
+
+		while j < len(typecol1):
+			if typecol1[j] == i:
+				amountcol[j] += DF['amount'][icol]
+				flag = True
+			j += 1
+		if not flag:
+			typecol1.append(i)
+			amountcol.append(DF['amount'][icol])
+
+		icol += 1
+
+	plt.pie(amountcol, labels=amountcol, shadow=5, autopct='%1.1f%%')
+	plt.title("Количество - Тип")
+	plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), shadow=True, ncol=2, labels=typecol1)
+	plt.show()
+
+
+def PriceType():
+
+	icoll=0
+
+	typecol2=[] # список содержащий элементы type без повторений
+	pricecol=[] # список содержащий цену вейпов всех типов без повторений
+
+	for i in DF['type']:
+		j = 0
+		flag = False
+
+		while j < len(typecol2):
+			if typecol2[j] == i:
+				pricecol[j] += DF['price'][icoll]
+				flag = True
+			j += 1
+		if not flag:
+			typecol2.append(i)
+			pricecol.append(DF['price'][icoll])
+
+		icoll += 1
+
+	plt.pie(pricecol, labels=pricecol, shadow=5, autopct='%1.1f%%')
+	plt.title("Цена - Тип")
+	plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), shadow=True, ncol=2, labels=typecol2)
+	plt.show()
+
+
 
 
 
 main = Tk()
+main.config(bg='dimgray')
+main.geometry("250x150+600+300")
 
 
+def func(value):
+	if str(value) == "Цена - Имя":
+		PriceName()
+	elif str(value) == "Количество - Имя":
+		AmountName()
+	elif str(value) == "Количество - Тип":
+		AmountType()
+	elif str(value) == "Цена - тип":
+		PriceType()
 
-
-PriceNameButton = Button(main, text='Цена - Имя', background="gray", foreground="black", command=PriceName)
-PriceNameButton.pack(side=LEFT)
-
-AmountNameButton = Button(main, text='Количество - Имя', background="gray", foreground="black", command=AmountName)
-AmountNameButton.pack(side=LEFT)
-
-#AmountTypeButton = Button(main, text='Количество - Тип', background="gray", foreground="black", command=AmountType)
-#AmountTypeButton.pack(side=LEFT)
-
+options = ["Цена - Имя", "Количество - Имя", "Количество - Тип", "Цена - Тип"]
+var = StringVar()
+var.set("Выбрать график")
+drop = OptionMenu(main, var, *options, command=func).pack(expand=1)
 
 
 
